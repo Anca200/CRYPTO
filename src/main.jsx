2fce48465092c0c4038a1d22c2ce066aae3b01b8
Anca200 <anca2k17@gmail.com>
2024-06-24 18:03:10 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -9,11 +9,6 @@ import Coin from './pages/Coin.jsx';
 import CoinContextProvider from './context/CoinContext.jsx';
 import Footer from './components/Footer.jsx';
 
-const cors =  require('cors')
-app.use(cors({
- origin: "https://peaceful-pixie-d78816.netlify.app/",
- methods: ["GET", "POST", "PUT", "DELETE"]
-}))
 
 const router = createBrowserRouter ([
   {
